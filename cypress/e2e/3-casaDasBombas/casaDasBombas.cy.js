052419160d2396ebcf70be6a7d15fac9636d14f7
The-Nis <116385280+The-Nis@users.noreply.github.com>
2022-11-09 15:54:36 -0300

    up

--- cypress/e2e/3-casaDasBombas/casaDasBombas.cy.js
+++ cypress/e2e/3-casaDasBombas/casaDasBombas.cy.js
@@ -5,7 +5,6 @@ describe('Casa das Bombas', () => {
 
     it('Login Casa das Bombas', () => {
         cy.get('.q-my-md > .q-field__inner > .q-field__control').type('adm');
-        cy.get('[for="f_13fe6e53-6aaf-4f11-a08a-4323c97ae7d4"] > .q-field__inner > .q-field__control')
         // cy.get('input[formcontrolname="password"]').type('123');
     })
 
